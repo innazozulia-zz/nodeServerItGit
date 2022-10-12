603076f648f07e93692d83c4fa196295d3f260b8
inna <innazozulia@outlook.com>
2022-10-12 13:49:48 +0300

    mysql

--- db/app.js
+++ db/app.js
@@ -3,10 +3,15 @@ const mysql = require("mysql");
 // configuration
 // 192.168.64.2
 const conn = mysql.createConnection({
-  host: "itgid.mysql.tools", //127.0.0.1
-  user: "itgid_nodetest",
+  host: "localhost",
+  port: "8443",
+  user: "root",
   database: "itgid_nodecourse",
-  password: "A16gAi50YiB0",
+  password: "",
+  // host: "itgid.mysql.tools", //127.0.0.1
+  // user: "root",
+  // database: "itgid_node",
+  // password: " ",
 });
 
 conn.connect(function (err) {
@@ -16,3 +21,30 @@ conn.connect(function (err) {
     console.log("Подключение к серверу MySQL успешно установлено");
   }
 });
+
+// let query = "SELECT * FROM user";
+
+// conn.query(query, (err, result, field) => {
+//   console.log(err);
+//   console.log(result);
+//   // console.log(field);
+// });
+
+conn.end((err) => {
+  if (err) {
+    console.log(err);
+    return err;
+  } else {
+    console.log("Database ----- Close");
+  }
+});
+
+// my sql
+
+// CREATE TABLE user (
+// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+// firstname VARCHAR(30) NOT NULL,
+// lastname VARCHAR(30) NOT NULL,
+// email VARCHAR(50),
+// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// )
