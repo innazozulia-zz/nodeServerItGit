0742a7ae13249375267dca1ccd6a5cf8af51b701
inna <innazozulia@outlook.com>
2022-10-16 13:36:49 +0300

    added ejs, main logic

--- request/view.js
+++ request/view.js
@@ -8,16 +8,34 @@ app.set("view engine", "ejs");
 app.listen(3001);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "How To Set Up a GraphQL API Server in Node.js",
+      snippet:
+        "This article was originally written for DigitalOcean. In An Introduction to GraphQL, you learned that GraphQL is an open-source query language and runtime for APIs created to solve issues that are often experienced with traditional REST API systems.",
+    },
+    {
+      title:
+        "WebSockets and Node.js - testing WS and SockJS by building a web app",
+      snippet:
+        "This post will look at implementing a couple of low-level WebSockets libraries with Node.js as the WebSocket server. We’ll look at how each library is used, and why one might choose it for their project.",
+    },
+    {
+      title: "Building a Real-Time Webapp with Node.js and Socket.io",
+      snippet:
+        "In this blogpost we showcase a project we recently finished for National Democratic Institute, an NGO that supports democratic institutions and practices worldwide.",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 app.get("/blogs/create", (req, res) => {
-  res.render();
+  res.render("create", { title: "Create a new blog" });
 });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Error Page" });
 });
