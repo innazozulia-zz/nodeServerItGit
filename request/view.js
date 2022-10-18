8aabcb81e4d42fa26c480c076a0ddbc93fe45cc2
inna <innazozulia@outlook.com>
2022-10-18 14:39:22 +0300

    update data in MangoDB, create Schema, add some data to db

--- request/view.js
+++ request/view.js
@@ -11,9 +11,10 @@ const Blog = require("./models/blog");
 
 //declare connect to database mongo db
 const db =
-  "mongodb+srv://user:12345678qwe@nodejsblog.cnrchxj.mongodb.net/nodejsBlog?retryWrites=true&w=majority";
+  "mongodb+srv://inna:test12345@nodejsblog.9lx65fm.mongodb.net/blog?retryWrites=true&w=majority";
 mongoose
   .connect(db)
+  // .then((result) => console.log("connected to db"))
   .then((result) => app.listen(3001))
   .catch((err) => console.log(err));
 
@@ -28,23 +29,45 @@ app.use(express.static("public"));
 //morgen middleware
 app.use(morgan("dev"));
 
-// mongoose and mongo sandbox routs
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "about",
-    body: "more about my new blog",
-  });
-  // blog.save().then((result) => {});
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//manual adding data to db
+// mongoose and mongo sandbox routes
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "new blog 542",
+//     snippet: "about this blog Node js",
+//     body: "more about my new blog",
+//   });
+//   blog
+//     .save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// see all blogs
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+//see single blog by ID
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("634e8a8676c22ac5d5bfe834")
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 // native middleware check
 // app.use((req, res, next) => {
@@ -61,28 +84,39 @@ app.get("/add-blog", (req, res) => {
 // });
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "How To Set Up a GraphQL API Server in Node.js",
-      snippet:
-        "This article was originally written for DigitalOcean. In An Introduction to GraphQL, you learned that GraphQL is an open-source query language and runtime for APIs created to solve issues that are often experienced with traditional REST API systems.",
-    },
-    {
-      title:
-        "WebSockets and Node.js - testing WS and SockJS by building a web app",
-      snippet:
-        "This post will look at implementing a couple of low-level WebSockets libraries with Node.js as the WebSocket server. We’ll look at how each library is used, and why one might choose it for their project.",
-    },
-    {
-      title: "Building a Real-Time Webapp with Node.js and Socket.io",
-      snippet:
-        "In this blogpost we showcase a project we recently finished for National Democratic Institute, an NGO that supports democratic institutions and practices worldwide.",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
+  // const blogs = [
+  //   {
+  //     title: "How To Set Up a GraphQL API Server in Node.js",
+  //     snippet:
+  //       "This article was originally written for DigitalOcean. In An Introduction to GraphQL, you learned that GraphQL is an open-source query language and runtime for APIs created to solve issues that are often experienced with traditional REST API systems.",
+  //   },
+  //   {
+  //     title:
+  //       "WebSockets and Node.js - testing WS and SockJS by building a web app",
+  //     snippet:
+  //       "This post will look at implementing a couple of low-level WebSockets libraries with Node.js as the WebSocket server. We’ll look at how each library is used, and why one might choose it for their project.",
+  //   },
+  //   {
+  //     title: "Building a Real-Time Webapp with Node.js and Socket.io",
+  //     snippet:
+  //       "In this blogpost we showcase a project we recently finished for National Democratic Institute, an NGO that supports democratic institutions and practices worldwide.",
+  //   },
+  // ];
+  // res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
